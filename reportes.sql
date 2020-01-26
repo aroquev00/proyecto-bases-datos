518430daa7b059cc05cdd0d1ad43c2dfcd1e4237
aroquev00 <aroquev00@users.noreply.github.com>
2020-01-26 16:11:48 -0600

    Begin Reports queries

---
+++ reportes.sql
@@ -0,0 +1,5 @@
+-- 1. Nombre Paciente, NumConsultas
+-- 2. Nombre Medicamento, Num Veces Recetado de mayor a menor
+-- 3. Detalle de consultas de un paciente, como stored procedure
+-- Datos: fecha consulta, edad paciente, doctor, peea, nota clínica
+-- 4. Fecha inicio/fin Detalle paciente, nombre paciente, address, birthdate, todo lo del punto 4
