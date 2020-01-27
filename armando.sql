c51c8854f8432aecd77183c981be3ba6dfda667e
aroquev00 <aroquev00@users.noreply.github.com>
2020-01-26 21:09:28 -0600

    Add tables

--- armando.sql
+++ armando.sql
@@ -11,26 +11,37 @@ CREATE TABLE medicina (
     PRIMARY KEY (medicinaID)
 );
 
+-- falta el FK al paciente, o del paciente al historial
 CREATE TABLE historial (
     historialID INT,
     fechaUltimaConsulta DATE,
-    fechaActualizacion DATE,
+    fechaUltimaActualizacion DATE,
     horasEjercicio INT,
-    fumador BOOLEAN NOT NULL,
-    tomador BOOLEAN NOT NULL,
+    fumador BOOLEAN,
+    tomador BOOLEAN,
     horasSuenio INT,
     calidadSuenio VARCHAR(10),
     PRIMARY KEY (medicinaID)  
 );
 
 CREATE TABLE enfermedadPrevia (
-
+    enfermedadPreviaID INT,
+    fechaEnfermedad DATE,
+    -- tratamientos? multivalue
+    enfermedadID INT,
+    PRIMARY KEY (enfermedadPreviaID),
+    FOREIGN KEY (enfermedadID) REFERENCES enfermedad(enfermedadID)
 );
 
 CREATE TABLE enfermedad (
-
+    enfermedadID INT,
+    nombreEnfermedad VARCHAR(20),
+    PRIMARY KEY (enfermedadID)
 );
 
 CREATE TABLE sintoma (
-
+    sintomaID INT,
+    nombreSintoma VARCHAR(20),
+    descripcionSintoma VARCHAR(1000)
+    PRIMARY KEY (sintomaID)
 );
