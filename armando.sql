75fcd6f69be73c698ab88fda2dc7cb0df880d0fa
Nestor Rubio <nestorulo29@gmail.com>
2020-01-27 10:32:50 -0600

    Cambios-Nestor

--- armando.sql
+++ armando.sql
@@ -12,6 +12,13 @@ CREATE TABLE medicamento (
     PRIMARY KEY (medicamentoID)
 );
 
+CREATE TABLE reportan (
+    consultaID INT,
+    sintomaID INT,
+    FOREIGN KEY (consultaID) REFERENCES Consulta(consultaID),
+    FOREIGN KEY (sintomaID) REFERENCES Sintoma(sintomaID)
+);
+
 CREATE TABLE consultaRecetaMedicamento (
     consultaID INT,
     medicamentoID INT,
@@ -73,4 +80,46 @@ CREATE TABLE sintoma (
     nombreSintoma VARCHAR(20),
     descripcionSintoma VARCHAR(1000)
     PRIMARY KEY (sintomaID)
+);
+
+CREATE TABLE Paciente(
+  pacienteID int NOT NULL,
+  nombre varchar(20),
+  apellidoPaterno varchar(20),
+  apellidoMaterno varchar(20),
+  fechaNacimiento date,
+  genero char,
+  tipoSangre varchar(3),
+  PRIMARY KEY(pacienteID)
+  FOREIGN KEY(historialID) REFERENCES Historial(historalID),
+  FOREIGN KEY(numeroPoliza) REFERENCES SeguroMedico(numeroPoliza),
+  FOREIGN KEY(visitaID) REFERENCES Consulta(visitaID)
+);
+
+CREATE TABLE SeguroMedico(
+  numeroPoliza int,
+  vigencia date,
+  expedicion date,
+  compañia varchar(50),
+  limite int
+  PRIMARY KEY(numeroPoliza)
+);
+
+CREATE TABLE Consulta(
+  visitaID int,
+  fecha date,
+  peso double,
+  estatura double,
+  notaClinica varchar(500),
+  peea varchar(500),
+  PRIMARY KEY (visitaID),
+  FOREIGN KEY(numeroPoliza) REFERENCES SeguroMedico(numeroPoliza),FOREIGN KEY(doctorID) REFERENCES Doctor(doctorID)
+;)
+
+CREATE TABLE Doctor(
+  doctorID int,
+  cedula char(18),
+  nombre varchar(20),
+  apellido varchar(20)
+  PRIMARY KEY(doctorID)
 );
