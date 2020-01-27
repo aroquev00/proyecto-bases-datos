2bf7272c029b530432c553d60d345ed536659d88
aroquev00 <aroquev00@users.noreply.github.com>
2020-01-27 10:41:14 -0600

    Add medicines

--- datos.sql
+++ datos.sql
@@ -7,4 +7,17 @@ VALUES (1, 'Armando', 'Roque', 'Villasana', 'M', 'O+', '13/05/2000', '');
 INSERT INTO consulta ()
 VALUES ();
 
+INSERT INTO medicina (medicamentoID, nombre, presentacion)
+VALUES (1, 'Loratadina', '10 mg')
+
+INSERT INTO medicina (medicamentoID, nombre, presentacion)
+VALUES (2, 'Ibuprofeno', '200 mg')
+
+INSERT INTO medicina (medicamentoID, nombre, presentacion)
+VALUES (3, 'Amoxicilina', '500 mg')
+
+INSERT INTO medicina (medicamentoID, nombre, presentacion)
+VALUES (4, 'Omeprazol', '20 mg')
+
+
 
